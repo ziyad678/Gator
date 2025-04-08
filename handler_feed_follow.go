e83b8253f5d947922e9c4eaa6f8e5c036c58bfe5
Ziyad <ziyad.almubarak@gmail.com>
2025-04-08 14:47:20 +0300

    w

--- handler_feed_follow.go
+++ handler_feed_follow.go
@@ -12,17 +12,28 @@ import (
 )
 
 func handlerFollow(s *state, cmd command) error {
+	user, err := s.db.GetUser(context.Background(),s.config.CurrentUserName)
+	if err!=nil{
+		log.Printf("Can't get user %v from database\n",s.config.CurrentUserName)
+		return err
+	}
 	if len(cmd.Args) != 1 {
 		log.Println("Please enter a URL to follow")
 		return fmt.Errorf("usage: %s <URL>", cmd.Name)
 	}
-	user := database.CreateFeedF{
+	feed, err := s.db.GetFeedByURL(context.Background(),cmd.Args[0])
+	if err!=nil{
+		log.Printf("Can't get feed %v from database\n",s.config.CurrentUserName)
+		return err
+	}
+	feedFollow := database.CreateFeedFollowParams{
 		ID:        uuid.New(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
-		Name:      cmd.Args[0],
+		UserID:      user.ID,
+		FeedID: feed.ID,
 	}
-	userDB, err := s.db.CreateUser(context.Background(), user)
+	userDB, err := s.db.CreateUser(context.Background(), user) 
 	if err != nil {
 		fmt.Println("Failed to create user. User already exists")
 		log.Printf("failed to create user %v in database. %v\n", user, err)
